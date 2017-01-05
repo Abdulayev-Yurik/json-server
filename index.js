4b16f811d029d70513479a199ed381ee606e3ef6
Yurik Abdulayev <yuriia@softwareplanet.com>
2017-01-05 17:25:42 +0200

    add hook

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-var http = require('http')
-var createHandler = require('gitlab-webhook-handler')
-var handler = createHandler({ path: '/webhook' })
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-})
+app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
 
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-handler.on('merge_request', function (event) {
-  console.log('ok')
-})
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/webhook', function(request, response) {
+  console.log("ok")
+});
+// var http = require('http')
+// var createHandler = require('gitlab-webhook-handler')
+// var handler = createHandler({ path: '/webhook' })
+//
+// http.createServer(function (req, res) {
+//   handler(req, res, function (err) {
+//     res.statusCode = 404
+//     res.end('no such location')
+//   })
+// }).listen(7777)
+//
+//
+// handler.on('error', function (err) {
+//   console.error('Error:', err.message)
+// })
+//
+// handler.on('merge_request', function (event) {
+//   console.log('ok')
+// })
